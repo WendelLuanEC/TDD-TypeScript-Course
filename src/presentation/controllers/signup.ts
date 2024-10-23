785bbbe024cd7892730701aca7aea78b1bbef8fd
WendelLuanEC <wendelluan@alu.ufc.br>
2024-10-23 14:38:28 -0300

    refactor: simplifi code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
     handle(httpRequest: httpRequest): httpResponse {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissimParamError('name'))
-    }
-        if (!httpRequest.body.email) {
-            return badRequest(new MissimParamError('email'))
+        const requiredFields = ['name', 'email']
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissimParamError(field))
+            }
         }
     }
 }
