b6bd9abd890a0c35efde2b1b78db432bdaccfeca
WendelLuanEC <wendelluan@alu.ufc.br>
2024-10-23 14:43:08 -0300

    test: ensure: SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -50,17 +50,17 @@ describe('SignUp Controller', () => {
 })
 
 describe('SignUp Controller', () => {
-    test('Should return 400 if no email is provided', () => {
+    test('Should return 400 if no password confirmation is provided', () => {
         const sut = new SignUpController()
         const httpRequest = {
             body: {
                 name: 'any_name',
                 password: 'any_password',
-                passwordConfirmation: 'any_password'
+                email: 'any_email@email.com',
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissimParamError('email'))
+        expect(httpResponse.body).toEqual(new MissimParamError('passwordConfirmation'))
     })
 })
