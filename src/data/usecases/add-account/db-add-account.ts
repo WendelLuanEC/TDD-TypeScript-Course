63c03a5ed3b6f4bc1cd9975cf485c0dd57f21346
WendelLuanEC <wendelluan@alu.ufc.br>
2024-11-14 14:31:26 -0300

    feat: ensure DbAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,8 +11,9 @@ export class DbAddAccount implements AddAccount {
     }
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const hashed_password = await this.encrypter.encrypt(accountData.password)
-        await this.addAccountRepository.add(Object.assign(Object.assign({}, accountData, { password: hashed_password })))
-        return new Promise(resolve => resolve(null))
+        const account = await this.addAccountRepository.add(Object.assign(Object.assign({}, accountData, { password: hashed_password })))
+        console.log(account)
+        return account
     }
 
 }
