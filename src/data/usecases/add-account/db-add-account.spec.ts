88da6fdf13a4a37dc136eefc4b280e204ecc561b
WendelLuanEC <wendelluan@alu.ufc.br>
2024-11-14 14:21:05 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -84,4 +84,16 @@ describe('DbAddAccount Usecase', () => {
         })
     })
 
+    test('Should Throw if AddAccountRepository Throws', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut()
+        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_password',
+            password: 'valid_password'
+        }
+        const promise = sut.add(accountData)
+        await expect(promise).rejects.toThrow()
+    })
+
 })
